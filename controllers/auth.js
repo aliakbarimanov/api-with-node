ebe8f8e6266ab87c4d8318fb35de7cc10d44a834
aliakbarimanov <aliakbar.imanov@gmail.com>
2024-02-25 17:54:00 +0400

    Create Login Controllers Auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,7 @@ const register = async (req, res) => {
         const passwordHash = await bcrypt.hash(password, 12);
 
         const newUser = await Auth.create({ username, email, password: passwordHash })
-        const userToken = await jwt.sign({ id: newUser.id }, process.env.SECRET_TOKEN, { expiresIn: "1h" });
+        const userToken = jwt.sign({ id: newUser.id }, process.env.SECRET_TOKEN, { expiresIn: "1h" });
 
         res.status(201).json({
             status: "OK",
@@ -27,6 +27,36 @@ const register = async (req, res) => {
         });
 
     } catch (error) {
-
+        return res.status(500).json({ message: error.message });
     }
 }
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await Auth.findOne({ email });
+
+        if (!user) {
+            return res.status(500).json({ message: "User is not find!" });
+        }
+
+        const comparePassword = await bcrypt.compare(password, user.password);
+
+        if (!comparePassword) {
+            return res.status(500).json({ message: "Password is false!" });
+        }
+
+        const token = jwt.sign({ id: user.id }, process.env.SECRET_TOKEN, { expiresIn: "1h" });
+
+        res.status(200).json({
+            status: "OK",
+            user,
+            token,
+        });
+
+    } catch (error) {
+        return res.status(500).json({message: error.message});
+    }
+}
+
+module.exports = {register, login};
