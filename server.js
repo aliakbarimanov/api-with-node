dd20ddb43f93b3e4e02cc51a2af5b8dd4b74d276
aliakbarimanov <aliakbar.imanov@gmail.com>
2024-07-09 03:08:19 +0400

    Created with express, nodemon, dotenv libraries

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Wow! This is working..");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.get("/users", (req, res) => {
+    res.send(`Port is running on`);
+});
+
+app.listen(port, () => console.log(`Port is running on ${port}`));
