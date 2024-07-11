af42b0014c484677f80ec995ae0c64bd11a376fd
aliakbarimanov <aliakbar.imanov@gmail.com>
2024-07-12 03:02:40 +0400

    Create Routes

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5001;
 
-app.get("/users", (req, res) => {
-    res.send(`Port is running on`);
-});
+// app.get("/users", (req, res) => {
+//     res.send(`Port is running on ${port}`);
+// });
+
+app.use("/contacts", require("./routes/contact-routes"));
 
 app.listen(port, () => console.log(`Port is running on ${port}`));
