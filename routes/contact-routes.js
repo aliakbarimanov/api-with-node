137e4b5a73dd93059360adcff217874e700122fc
aliakbarimanov <aliakbar.imanov@gmail.com>
2024-07-17 02:31:57 +0400

    Create routes for CRUD

--- routes/contact-routes.js
+++ routes/contact-routes.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-router.route("/", (req, res) => {
+router.route("/").get((req, res) => {
     res.status(200).json({ message: "Get all contacts!" });
+});
+
+router.route("/:id").get((req, res) => {
+    res.status(200).json({ message: `Get contact for ${req.params.id} id.` });
+});
+
+router.route("/").post((req, res) => {
+    res.status(200).json({ message: "Post contact successfully!" });
+});
+
+router.route("/:id").delete((req, res) => {
+    res.status(200).json({ message: `Delete contact for ${req.params.id} id.` });
+});
+
+router.route("/:id").put((req, res) => {
+    res.status(200).json({ message: `Edit contact for ${req.params.id} id.` });
 });
 
 module.exports = router;
