179aa662201db018a2d0985f77b86bd99a1f737c
aliakbarimanov <aliakbar.imanov@gmail.com>
2024-07-18 04:48:55 +0400

    Create middleware

--- controllers/contact-controllers.js
+++ controllers/contact-controllers.js
@@ -1,22 +1,27 @@
-const getContacts = (req, res) => {
+const asyncHandler = require("express-async-handler");
+
+const getContacts = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Get contacts with controller." });
-};
+});
 
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Get contact with controller for ${req.params.id} id` });
-};
+});
 
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
+
+    console.log("The req body is:", req.body);
+
     res.status(200).json({ message: "Create contact with controller." });
-};
+});
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update contact with controller for ${req.params.id} id` });
-};
+});
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete contact with controller for ${req.params.id} id.` });
-};
+});
 
 module.exports = {
     getContacts,
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5001;
+const errorHandler = require("./middleware/error-handler");
+const dotenv = require("dotenv").config();
+
+app.use(express.json());
+app.use("/contacts", require("./routes/contact-routes"));
+app.use(errorHandler);
+
+
+app.listen(port, () => console.log(`Port is running on ${port}`));
+
 
 // app.get("/users", (req, res) => {
 //     res.send(`Port is running on ${port}`);
 // });
-
-app.use("/contacts", require("./routes/contact-routes"));
-
-app.listen(port, () => console.log(`Port is running on ${port}`));
