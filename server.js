cdc600eeaa665d831ba40831d586b6a364ea26b1
aliakbarimanov <aliakbar.imanov@gmail.com>
2024-07-21 07:18:06 +0400

    Create user route and controller

--- server.js
+++ server.js
@@ -10,12 +10,8 @@ const port = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use("/contacts", require("./routes/contact-routes"));
+app.use("/user", require("./routes/user-routes"));
 app.use(errorHandler);
 
 
 app.listen(port, () => console.log(`Port is running on ${port}`));
-
-
-// app.get("/users", (req, res) => {
-//     res.send(`Port is running on ${port}`);
-// });
